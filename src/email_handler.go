a64be2200248a1a644a690206a653338f83a1766
swandrn <159421702+swandrn@users.noreply.github.com>
2024-07-05 20:03:55 +0200

    Initial commit

---
+++ src/email_handler.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/jordan-wright/email"
+)
+
+func sendEmail(address string, name string) error {
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+
+	emailToSend := email.NewEmail()
+	emailToSend.From = os.Getenv("EMAIL_USER")
+	emailToSend.To = []string{address}
+	emailToSend.Subject = "Vous avez des tâches à compléter !"
+	emailToSend.Text = []byte(
+		fmt.Sprintf(`Bonjour %s !
+		Vous avez des tâches non complétés !`, name),
+	)
+
+	//Change password
+	err = emailToSend.Send("smtp.mail.yahoo.com:587", smtp.PlainAuth("", os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_APP_PWD"), "smtp.mail.yahoo.com"))
+	if err != nil {
+		return err
+	}
+	return nil
+}
